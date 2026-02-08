3684304cbff7cf3acc186516cc87d86f8079fbbc
Rohit4Real <rohitknambiar@gmail.com>
2026-02-08 20:54:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
   const envelope = $("#envelope");
   const song = document.getElementById("loveSong");
+  const heartContainer = document.getElementById("floating-hearts");
 
   envelope.on("click", function () {
-    envelope.addClass("open").removeClass("close");
+    envelope.addClass("open");
 
     song.volume = 0.6;
     song.play().catch(() => {});
+
+    startHearts();
   });
+
+  function startHearts() {
+    setInterval(() => {
+      const heart = document.createElement("div");
+      heart.className = "heart";
+      heart.style.left = Math.random() * 100 + "vw";
+      heart.style.animationDuration = 4 + Math.random() * 3 + "s";
+
+      heartContainer.appendChild(heart);
+
+      setTimeout(() => {
+        heart.remove();
+      }, 7000);
+    }, 300);
+  }
 });
